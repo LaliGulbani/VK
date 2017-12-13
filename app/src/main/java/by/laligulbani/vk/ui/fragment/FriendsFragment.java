5159ce46eca5100f12f0dc4bac034d3ee3435ad5
Lali Gulbani <ilal9711@tut.by>
2017-12-13 18:38:14 +0300

    little fix

--- app/src/main/java/by/laligulbani/vk/ui/fragment/FriendsFragment.java
+++ app/src/main/java/by/laligulbani/vk/ui/fragment/FriendsFragment.java
@@ -2,14 +2,23 @@ package by.laligulbani.vk.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import by.laligulbani.vk.R;
+import by.laligulbani.vk.ui.activity.ViewPagerAdapter;
 
 public class FriendsFragment extends Fragment {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +28,32 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.profile, container, false);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
-}
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FriendsFragment(), "Все друзья");
+        adapter.addFragment(new FriendsFragment(), "Онлайн");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    }
+
